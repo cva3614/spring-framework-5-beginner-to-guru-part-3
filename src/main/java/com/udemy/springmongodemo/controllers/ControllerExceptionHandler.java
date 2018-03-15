5f507c85581bf5023594ccf3efda7a7673912031
js <jsbsmp@gmail.com>
2018-03-15 16:51:55 +0200

    Reactive Programming with Spring and MongoDB: WebFlux Controller Advice

--- src/main/java/com/udemy/springmongodemo/controllers/ControllerExceptionHandler.java
+++ src/main/java/com/udemy/springmongodemo/controllers/ControllerExceptionHandler.java
@@ -1,9 +1,23 @@
 package com.udemy.springmongodemo.controllers;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.support.WebExchangeBindException;
 
 @Slf4j
 @ControllerAdvice
 public class ControllerExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(WebExchangeBindException.class)
+    public String handleNumberFormat(Exception exception, Model model) {
+        log.error("Handling Binding Exception");
+        log.error(exception.getMessage());
+        model.addAttribute("exception", exception);
+        return "400error";
+    }
 }
