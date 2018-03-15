2d09254d088914ff448f6e668f3d9f0c2375653d
js <jsbsmp@gmail.com>
2018-03-15 16:59:57 +0200

    Reactive Programming with Spring and MongoDB: Router Functions

---
+++ src/main/java/com/udemy/springmongodemo/config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.udemy.springmongodemo.config;
+
+import com.udemy.springmongodemo.domain.Recipe;
+import com.udemy.springmongodemo.services.RecipeService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    RouterFunction<?> routes(RecipeService recipeService) {
+        return RouterFunctions.route(GET("/api/recipes"),
+                serverRequest -> ServerResponse
+                        .ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(recipeService.getRecipes(), Recipe.class));
+    }
+}
